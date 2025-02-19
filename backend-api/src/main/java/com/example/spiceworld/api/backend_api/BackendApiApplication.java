df1c682d73249484ad10e5a4e0d0a7c93cf73576
sahilsOfficialCode <sahilsajith.official@gmail.com>
2025-02-19 18:57:54 +0530

    main code

--- backend-api/src/main/java/com/example/spiceworld/api/backend_api/BackendApiApplication.java
+++ backend-api/src/main/java/com/example/spiceworld/api/backend_api/BackendApiApplication.java
@@ -2,7 +2,7 @@ package com.example.spiceworld.api.backend_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//main page
 @SpringBootApplication
 public class BackendApiApplication {
 
